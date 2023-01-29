64650be37bb8e723d84ace52c195b7274a66c96b
oyunseong <42116216+oyunseong@users.noreply.github.com>
2023-01-29 21:38:20 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,7 +1,10 @@
 n = int(input())
-arr= [0,1]
 
-for i in range(n):
-    arr.append(arr[-1] + arr[-2])
-   
+arr = [0] * 91
+arr[0] = 0
+arr[1] = 1
+
+for i in range(89):
+    arr[i + 2] = arr[i + 1] + arr[i]
+        
 print(arr[n])
