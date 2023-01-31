bfe775a69612996dd217ad3d5867862cdcd6fc67
oyunseong <42116216+oyunseong@users.noreply.github.com>
2023-01-31 19:59:38 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 248 ms, Memory: 41316 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    dp[i] = arr[i - 1] + dp[i - 1]
+
+for i in range(m):
+    x, y = list(map(int, input().split()))
+    print(dp[y] - dp[x - 1])
