cde9c92767a9e9f399f276943958cfce3c5a4953
oyunseong <42116216+oyunseong@users.noreply.github.com>
2023-02-02 03:12:22 +0900

    [Gold V] Title: 동전 2, Time: 376 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+dp = [10001] * (k + 1)
+dp[0] = 0
+
+coin = []
+for i in range(n):
+    coin.append(int(input()))
+
+for c in coin:
+    for i in range(c, k + 1):
+        dp[i] = min(dp[i], dp[i - c] + 1)
+if dp[k] == 10001:
+    print(-1)
+else:
+    print(dp[k])
