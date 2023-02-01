145e9e62ae00008599643aaa79ba903f81a2de5b
oyunseong <42116216+oyunseong@users.noreply.github.com>
2023-02-02 01:20:31 +0900

    [Gold V] Title: 동전 1, Time: 180 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+n, k = map(int, input().split())
+
+coin = []
+dp = [0] * (k + 1)
+for _ in range(n):
+    coin.append(int(input()))
+
+coin.sort()
+dp[0] = 1
+
+for c in coin:
+    for i in range(c, k + 1):
+        dp[i] += dp[i - c]
+print(dp[k])
